667808c8764949f56618b85fec43ab47dff38f87
Anthony Olukitibi <anthony.olukitibi@verizonconnect.com>
2021-12-08 14:27:23 -0500

    Initial Commit

---
+++ GeekSeat/GeekSeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeekSeatApp.swift
+//  GeekSeat
+//
+//  Created by Anthony Olukitibi on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct GeekSeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
